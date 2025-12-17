58eb2c95edf35d4ce40304758278c70ab370d9ed
abhaydeep2007 <abhaydeep2007@gmail.com>
2025-12-17 12:42:40 +0530

    Add files via upload

---
+++ Greetings.py
@@ -0,0 +1,9 @@
+# Take input from the user
+first_name = input("Enter your first name: ")
+last_name = input("Enter your last name: ")
+
+# Combine first name and last name
+full_name = first_name + " " + last_name
+
+# Display personalized greeting
+print("Hello,", full_name + "! Welcome to the Python program")
